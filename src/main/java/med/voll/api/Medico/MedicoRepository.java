c58906cf34004fda46000231b55adc33ae503a22
Jamile Matos dos Reis <140525086+JamileReis@users.noreply.github.com>
2024-10-27 11:23:12 -0300

    new push

--- src/main/java/med/voll/api/Medico/MedicoRepository.java
+++ src/main/java/med/voll/api/Medico/MedicoRepository.java
@@ -1,5 +1,7 @@
 package med.voll.api.Medico;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,4 +10,6 @@ import java.util.List;
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
     List<Medico> findAllByAtivo(Boolean ativo);
 
+
+    Page<Medico> findAllByAtivoTrue(Pageable paginacao);
 }
