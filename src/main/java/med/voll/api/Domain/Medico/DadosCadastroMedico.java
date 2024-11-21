c904539c2a26f223df5025d918a56d3296573896
Jamile Matos dos Reis <140525086+JamileReis@users.noreply.github.com>
2024-11-21 20:55:35 -0300

    new push

--- src/main/java/med/voll/api/Domain/Medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/Domain/Medico/DadosCadastroMedico.java
@@ -1,4 +1,4 @@
-package med.voll.api.Medico;
+package med.voll.api.Domain.Medico;
 
 
 import jakarta.validation.Valid;
@@ -6,7 +6,8 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-import med.voll.api.Endereco.DadosEndereco;
+import med.voll.api.Domain.Endereco.DadosEndereco;
+
 
 public record DadosCadastroMedico(
 
