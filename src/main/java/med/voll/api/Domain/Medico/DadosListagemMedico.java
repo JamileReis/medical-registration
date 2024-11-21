77adcabed09bf465701889ad247dad3fd9c2ceb6
Jamile Matos dos Reis <140525086+JamileReis@users.noreply.github.com>
2024-11-21 20:57:07 -0300

    new push

--- src/main/java/med/voll/api/Domain/Medico/DadosListagemMedico.java
+++ src/main/java/med/voll/api/Domain/Medico/DadosListagemMedico.java
@@ -1,4 +1,4 @@
-package med.voll.api.Medico;
+package med.voll.api.Domain.Medico;
 
 public record DadosListagemMedico(Long id,String nome, String email, String crm, Especialidade especialidade) {
 
