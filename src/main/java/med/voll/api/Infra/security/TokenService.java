9b06338d4fb7a88111fce93a85950e984ae365bb
Jamile Matos dos Reis <140525086+JamileReis@users.noreply.github.com>
2025-02-18 13:22:14 -0300

    security filter created

--- src/main/java/med/voll/api/Infra/security/TokenService.java
+++ src/main/java/med/voll/api/Infra/security/TokenService.java
@@ -1,8 +1,10 @@
 package med.voll.api.Infra.security;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import med.voll.api.Domain.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -29,6 +31,23 @@ public class TokenService {
     } catch (JWTCreationException exception) {
       throw new RuntimeException("Erro ao gerar token JWT", exception);
 
+    }
+  }
+
+  public String getSubject(String tokenJWT) {
+    try {
+      if (secret == null || secret.isEmpty()) {
+        throw new IllegalStateException("Chave secreta do JWT não está configurada corretamente!");
+      }
+
+      Algorithm algoritmo = Algorithm.HMAC256(secret);
+      JWTVerifier verifier = JWT.require(algoritmo)
+              .withIssuer("API Voll Med")
+              .build();
+
+      return verifier.verify(tokenJWT).getSubject();
+    } catch (JWTVerificationException exception) {
+      throw new RuntimeException("Token JWT inválido ou expirado!", exception);
     }
   }
 
