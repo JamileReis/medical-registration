6a808e0672c91062f72b0baec1bb7aa7b176ae45
Jamile Matos dos Reis <140525086+JamileReis@users.noreply.github.com>
2025-05-21 12:42:39 -0300

    security filter created

---
+++ src/main/java/med/voll/api/Domain/Consulta/Validacoes/ValidadorAgendamentosDeConsulta.java
@@ -0,0 +1,10 @@
+package med.voll.api.Domain.Consulta.Validacoes;
+
+import med.voll.api.Domain.Consulta.DadosAgendamentoConsulta;
+
+public class ValidadorAgendamentosDeConsulta {
+
+    public void validar(DadosAgendamentoConsulta dados) {
+
+    }
+}
---
+++ src/main/java/med/voll/api/Domain/ValidacaoException.java
@@ -0,0 +1,7 @@
+package med.voll.api.Domain;
+
+public class ValidacaoException extends RuntimeException {
+    public ValidacaoException(String mensagem) {
+        super(mensagem);
+    }
+}
